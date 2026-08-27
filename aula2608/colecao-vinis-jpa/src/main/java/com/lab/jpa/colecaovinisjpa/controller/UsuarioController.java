5bb8569a2c966299ce3f301929993e815f888f35
Maromo <professormoraes@gmail.com>
2026-08-27 12:05:19 -0300

    Aula do dia 26 de agosto

---
+++ aula2608/colecao-vinis-jpa/src/main/java/com/lab/jpa/colecaovinisjpa/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.lab.jpa.colecaovinisjpa.controller;
+import com.lab.jpa.colecaovinisjpa.model.Usuario;
+import com.lab.jpa.colecaovinisjpa.repository.UsuarioRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    private final UsuarioRepository repository;
+
+    public UsuarioController(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+    @PostMapping
+    @ResponseBody
+    public Usuario criar(@RequestBody Usuario usuario){
+        return repository.save(usuario);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<Usuario>listar(){
+        return repository.findAll();
+    }
+}
